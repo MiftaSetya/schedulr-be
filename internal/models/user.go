556432233e5e88b64a52eb2698955b60be927f04
Mifta Setya <mifta@archlinux.localdomain>
2025-09-24 22:20:19 +0700

    feat: create user model

--- internal/models/user.go
+++ internal/models/user.go
@@ -1,1 +1,10 @@
 package models
+
+import "gorm.io/gorm"
+
+type User struct {
+	gorm.Model
+	Name     string `gorm:"size:100;not null" json:"name"`
+	Email    string `gorm:"size:150;unique;not null" json:"email"`
+	Password string `gorm:"size:250;not null" json:"-"`
+}
