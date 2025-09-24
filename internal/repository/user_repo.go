57d4e33f6549e8fe57b9c7f2b9cbd3374c74d10f
Mifta Setya <mifta@archlinux.localdomain>
2025-09-24 22:46:05 +0700

    feat: create user repo

--- internal/repository/user_repo.go
+++ internal/repository/user_repo.go
@@ -1,1 +1,27 @@
 package repository
+
+import (
+	"schedulr/internal/models"
+
+	"gorm.io/gorm"
+)
+
+type UserRepository struct {
+	DB *gorm.DB
+}
+
+func (r *UserRepository) CreateUser(user *models.User) error {
+	return r.DB.Create(user).Error
+}
+
+func (r *UserRepository) GetUserByEmail(email string) (*models.User, error) {
+	user := &models.User{}
+	err := r.DB.Where("email = ?", email).First(user).Error
+	return user, err
+}
+
+func (r *UserRepository) GetUserById(id uint) (*models.User, error) {
+	user := &models.User{}
+	err := r.DB.First(user, id).Error
+	return user, err
+}
