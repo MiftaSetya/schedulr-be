bc9ec23a246bdc376f1cd9d0aae35f997f60afb9
Mifta Setya <mifta@archlinux.localdomain>
2025-09-25 06:58:21 +0700

    feat: create auth routes

--- internal/api/routes/auth_routes.go
+++ internal/api/routes/auth_routes.go
@@ -1,1 +1,15 @@
 package routes
+
+import (
+	"schedulr/internal/api/handler"
+
+	"github.com/gin-gonic/gin"
+)
+
+func AuthRoutes(r *gin.Engine, authHandler *handler.AuthHandler) {
+	auth := r.Group("/auth")
+	{
+		auth.POST("/register", authHandler.Register)
+		auth.POST("/login", authHandler.Login)
+	}
+}
