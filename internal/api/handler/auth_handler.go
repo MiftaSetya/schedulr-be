24e2bd8df7144194cb569d13575c3ce6fd8df62c
Mifta Setya <mifta@archlinux.localdomain>
2025-09-25 06:51:56 +0700

    feat: create auth handler

--- internal/api/handler/auth_handler.go
+++ internal/api/handler/auth_handler.go
@@ -1,1 +1,68 @@
 package handler
+
+import (
+	"net/http"
+	"schedulr/internal/models"
+	"schedulr/internal/service"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
+)
+
+type AuthHandler struct {
+	Service   *service.AuthService
+	JwtSecret string
+}
+
+func (h *AuthHandler) Register(c *gin.Context) {
+	var input models.User
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	if err := h.Service.Register(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{"message": "user registered"})
+}
+
+func (h *AuthHandler) Login(c *gin.Context) {
+	var input struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	user, err := h.Service.Login(input.Email, input.Password)
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": err.Error()})
+		return
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"user_id": user.ID,
+		"exp":     time.Now().Add(30 * 24 * time.Hour).Unix(),
+	})
+
+	tokenString, err := token.SignedString([]byte(h.JwtSecret))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "could not generate token"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"user": gin.H{
+			"id":    user.ID,
+			"name":  user.Name,
+			"email": user.Email,
+		},
+		"token": tokenString,
+	})
+}
