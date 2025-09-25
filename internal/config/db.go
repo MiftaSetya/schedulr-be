042a243defb57c832d9b77c8291d557a471a9c27
Mifta Setya <mifta@archlinux.localdomain>
2025-09-25 07:04:45 +0700

    feat: setup db

--- internal/config/db.go
+++ internal/config/db.go
@@ -1,1 +1,14 @@
 package config
+
+import (
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func InitDB() (*gorm.DB, error) {
+	dsn := os.Getenv("DATABASE_URL")
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	return db, err
+}
